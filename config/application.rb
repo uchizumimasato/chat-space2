9a9c0290188f04d62951e709d231d9fae038fa73
utcizumi masato <24700269+uchizumimasato@users.noreply.github.com>
2019-01-30 11:28:06 +0900

    change location

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module ChatSpace2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
   end
 end
